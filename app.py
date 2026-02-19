2c6c59f5755d1968e227800146087718219c2e8f
diegoaraujogt-tech <diego.araujogt@gmail.com>
2026-02-19 15:48:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def get_drive_service():
 
     credentials = service_account.Credentials.from_service_account_info(
         creds_dict,
-        scopes=['https://www.googleapis.com/auth/drive.file']
+        scopes=['https://www.googleapis.com/auth/drive']
     )
     return build('drive', 'v3', credentials=credentials)
 
